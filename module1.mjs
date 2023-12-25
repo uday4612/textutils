1d98b6e9c096965ea501229465e26f11cc349e08
Patel Uday <78943653+uday4612@users.noreply.github.com>
2023-12-25 22:49:08 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+const a = "Uday";
+const b = "Patel";
+const c = "Piya";
+const d = "om";
+
+export default c;
+export{a};
+export{b};
+export{d};
---
+++ module2.mjs
@@ -0,0 +1,4 @@
+import dz, {a,b,d} from './module1.mjs'
+console.log(dz);
+console.log(b);
+console.log(a);
